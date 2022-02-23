83d2e161035c19d54338a445920b3e3945e5c309
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-02-23 13:09:22 -0300

    refactor: refatorado para utilizar arrays ao inves de arraylist

--- src/br/fai/ex06/Main.java
+++ src/br/fai/ex06/Main.java
@@ -1,5 +1,4 @@
 package br.fai.ex06;
-import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
@@ -7,25 +6,25 @@ public class Main {
 		app.start();
 	}
 	private void start(){
-		ArrayList<String> carros = new ArrayList<String>();
-		carros.add("civic");
-		carros.add("gol");
-		carros.add("palio");
-		carros.add("uno");
+		String carros[] = {"civic", "gol", "palio", "uno"};
+		int tamCarros = carros.length;
+		String novaListaCarros[] = new String[tamCarros + 1];
+			
 		System.out.println("Lista original");
 		exibeArray(carros);
-		carros.set(1, "L200");
+		carros[1] = "L200"; 
 		System.out.println("Lista nova");
 		exibeArray(carros);
-		carros.add("gol");
+		novaListaCarros = carros;
+		novaListaCarros[tamCarros - 1] = "gol";
 		System.out.println("Lista nova");
 		exibeArray(carros);
 	}
 	
-	private void exibeArray(ArrayList<String> carros) {
-		carros.forEach(carro -> {
+	private void exibeArray(String carros[]) {
+		for (String carro : carros) {
 			System.out.println(carro);
-		});
+		}
 	}
 	
 }
